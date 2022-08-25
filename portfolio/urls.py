f73ff18b76cb68a2347e1cce91033b458a2d69b4
philippe <pythonjsdev@gmail.com>
2022-08-25 20:37:01 +0200

    Refactorisation et logo

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,13 +4,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from projects.views import home, project_detail, web, other
+from projects.views import home, project_detail, apps, projects_web, others_projects
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('web', web, name='web'),
-    path('other', other, name='other'),
+    path('apps/<str:category>/', apps, name='apps'),
+    path('projects/<str:framework>', projects_web, name='projects'),
+    path('<str:language>', others_projects, name='others-projects'),
     path('project/<str:slug>/', project_detail, name='project-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from projects.models import Project
 
+WEB = ['javascript', 'python', 'django', 'drf', 'flask']
+LANGUAGES = ['javascript', 'python']
+
 
 def home(request):
     url_oc = "https://openclassrooms.com/fr/paths/518-developpeur-dapplication-python"
@@ -20,15 +23,36 @@ def project_detail(request, slug):
                            'sub_title': sub_title})
 
 
-def web(request):
-    projects = Project.objects.filter(category=Project.Categories.WEB_APP)
-    title = 'Applications Web'
+def apps(request, category):
+    if category == 'web':
+        title = 'Applications Web'
+        projects = filter_by_keyword('WEB_APP', WEB)
+    else:
+        title = 'Autres Applications'
+        projects = filter_by_keyword('OTHERS', LANGUAGES)
+    projects = {k: projects[k] for k in sorted(projects.keys())}
     return render(request, 'projects/apps.html', context={'projects': projects,
+                                                          'category': category,
                                                           'title': title})
 
 
-def other(request):
-    projects = Project.objects.filter(category=Project.Categories.OTHERS)
-    title = 'Autres Applications'
-    return render(request, 'projects/apps.html', context={'projects': projects,
-                                                          'title': title})
+def projects_web(request, framework):
+    projects = Project.objects.filter(keywords_list__icontains=framework).filter(category='W').order_by('name')
+    title = framework.upper() if framework in LANGUAGES else 'Framework ' + framework.upper()
+    return render(request, 'projects/projects.html', context={'projects': projects,
+                                                              'title': title})
+
+
+def others_projects(request, language):
+    projects = Project.objects.filter(keywords_list__icontains=language).filter(category='O').order_by('name')
+    title = language.upper()
+    return render(request, 'projects/projects.html', context={'projects': projects,
+                                                              'title': title})
+
+
+def filter_by_keyword(category_app, keywords):
+    if category_app == 'WEB_APP':
+        projects = Project.objects.filter(category=Project.Categories.WEB_APP).order_by('name')
+    else:
+        projects = Project.objects.filter(category=Project.Categories.OTHERS).order_by('name')
+    return {k: projects.filter(keywords_list__icontains=k) for k in keywords}
